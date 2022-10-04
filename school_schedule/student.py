cea126d4ce912792eef26a80e019afe1043c4e63
Emily <sonemily+github@gmail.com>
2022-10-04 13:54:47 -0400

    Create Test 2.

--- school_schedule/student.py
+++ school_schedule/student.py
@@ -4,11 +4,15 @@ class Student:
         self.grade = grade
         self.classes = classes
 
-    def add_class(self,class_name):
+    def add_class(self, class_name):
         self.classes.append(class_name)
+        return self.classes
 
     def get_num_classes(self):
         return len(self.classes)
 
+    def display_classes(self):
+        return(", ".join(self.classes))
+
     def summary(self):
-        return f"{self.name} is a {self.grade} enrolled in {self.get_num_classes}"
+        return (f"{self.name} is a {self.grade} enrolled in {self.get_num_classes()} classes : {self.display_classes()}")
--- tests/test_student.py
+++ tests/test_student.py
@@ -14,3 +14,17 @@ def test_add_class_increases_classes_length():
 
     assert len(diana.classes) == 4
     assert "Minnow Politics" in diana.classes
+
+
+def test_display_classes_returns_string_of_classes():
+    name = "Diana"
+    grade = "sophomore"
+    classes = ["Astrophysics",
+        "Underwater Basket Weaving",
+        "How to Talk Like a Shark"]
+    
+    diana = Student(name, grade, classes)
+
+    result = diana.display_classes()
+
+    assert result == "Astrophysics, Underwater Basket Weaving, How to Talk Like a Shark"
